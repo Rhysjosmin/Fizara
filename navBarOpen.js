e3b94d6b28bc4ca423cd4588a7c481f9a51860e2
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-03-06 22:27:52 +0530

    mobile

--- navBarOpen.js
+++ navBarOpen.js
@@ -1,10 +1,24 @@
 const menuButton= document.getElementById('menuButton')
 const menu=document.getElementById('menu')
-let state=false
-
-window.onresize=()=>{
+let state=true
+if(window.innerWidth>500){
     menu.style.height='auto';
     menu.style.opacity=1
+}else{
+    menu.style.height='0';
+    menu.style.opacity=0
+}
+
+window.onresize=e=>{
+    // console.log(window.innerWidth)
+    if(window.innerWidth>500){
+        menu.style.height='auto';
+        menu.style.opacity=1
+    }else{
+        menu.style.height='0';
+        menu.style.opacity=0
+    }
+  
 
 }
 menuButton.onclick=()=>{

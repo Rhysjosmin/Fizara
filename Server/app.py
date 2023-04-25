a893fa89d6779f2657245ff85cda980366b89721
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-04-25 20:06:03 +0530

    Stuff

--- Server/app.py
+++ Server/app.py
@@ -94,30 +94,68 @@ NewsDB = {
         'Body': 'Gout, a common form of inflammatory arthritis, can cause intense pain, swelling, and stiffness in the joints.',
         'link': 'https://www.medicalnewstoday.com/articles/new-look-at-an-ancient-disease-study-finds-novel-treatment-targets-for-gout'
     },
-    'New study suggests potential link between air pollution and dementia': {
-        'Body': 'A recent study has found a potential link between exposure to air pollution and an increased risk of developing dementia. The study analyzed data from over 13,000 people in the UK and found that those living in areas with higher levels of air pollution were more likely to develop dementia than those in cleaner areas.',
-        'link': 'https://www.theguardian.com/society/2022/feb/21/air-pollution-linked-to-increased-risk-of-dementia-major-study-suggests'
+  "New research reveals potential link between coffee consumption and reduced risk of heart disease": {
+        "Body": "A new study suggests that drinking coffee may lower the risk of developing heart disease. The study found that people who drank two to three cups of coffee per day had a 20% lower risk of heart disease compared to those who did not drink coffee. The researchers believe that the antioxidants and other beneficial compounds in coffee may be responsible for this protective effect.",
+        "link": "https://www.medicalnewstoday.com/articles/new-research-reveals-potential-link-between-coffee-consumption-and-reduced-risk-of-heart-disease"
     },
-    'Scientists discover new speciesrt of ancient human in Israel': {
-        'Body': 'Scientists have discovered a new species of ancient human in Israel, dating back around 140,000 years. The fossils were found in a cave and are believed to belong to a previously unknown group of humans, who may have interbred with Neanderthals and modern humans.',
-        'link': 'https://www.bbc.com/news/science-environment-61654780'
+    "Scientists discover new species of deep-sea fish off the coast of Australia": {
+        "Body": "A team of scientists has discovered a new species of deep-sea fish off the coast of Australia. The fish, which has been named the 'Black-Blotched Stingray', was found at a depth of over 2,000 meters and has unique features such as its black and white coloration and a spine that is used for defense. The discovery is an exciting development for marine biology and highlights the need to continue exploring the depths of the ocean.",
+        "link": "https://www.sciencedaily.com/releases/2022/03/220328104006.htm"
     },
-    'Scientists discover new specierees of ancient human in Israel': {
-        'Body': 'Scientists have discovered a new species of ancient human in Israel, dating back around 140,000 years. The fossils were found in a cave and are believed to belong to a previously unknown group of humans, who may have interbred with Neanderthals and modern humans.',
-        'link': 'https://www.bbc.com/news/science-environment-61654780'
+    "Researchers find potential new treatment for type 2 diabetes": {
+        "Body": "Researchers have discovered a potential new treatment for type 2 diabetes. The treatment involves a drug that targets a specific protein in the liver, which has been found to play a key role in regulating blood sugar levels. In tests on mice, the drug was able to significantly lower blood sugar levels and improve insulin sensitivity. Further research is needed to determine whether the drug is safe and effective in humans, but the findings offer hope for those living with type 2 diabetes.",
+        "link": "https://www.medicalnewstoday.com/articles/researchers-find-potential-new-treatment-for-type-2-diabetes"
     },
-    'Scientists discover new speci77es of ancient human in Israel': {
-        'Body': 'Scientists have discovered a new species of ancient human in Israel, dating back around 140,000 years. The fossils were found in a cave and are believed to belong to a previously unknown group of humans, who may have interbred with Neanderthals and modern humans.',
-        'link': 'https://www.bbc.com/news/science-environment-61654780'
-    },
-    'Scientists discover new species o54654f ancient human in Israel': {
-        'Body': 'Scientists have discovered a new species of ancient human in Israel, dating back around 140,000 years. The fossils were found in a cave and are believed to belong to a previously unknown group of humans, who may have interbred with Neanderthals and modern humans.',
-        'link': 'https://www.bbc.com/news/science-environment-61654780'
-    },
+    "New study suggests link between screen time and increased risk of depression in teenagers": {
+        "Body": "A new study has found a potential link between high levels of screen time and an increased risk of depression in teenagers. The study followed over 1,000 teenagers for two years and found that those who spent more than four hours per day on screens were significantly more likely to develop symptoms of depression compared to those who spent less time on screens. The researchers suggest that this link may be due to the social isolation and lack of physical activity that often accompanies excessive screen time.",
+        "link": "https://www.theguardian.com/society/2022/mar/24/link-between-screen-time-and-depression-in-teenagers-study-suggests"
+    }
 }
 UserDatabase = {
 
     "john": {"email": "johndoc@doc.com", "password": "1234", "DocID": "12345"},
+ 
+ "james" : {'email': 'james@icloud.com', 'password': '425932', 'DocID': '428809'},
+
+"john" : {'email': 'john@icloud.com', 'password': '591493', 'DocID': '415132'},
+
+"jennifer" : {'email': 'jennifer@gmail.com', 'password': '972472', 'DocID': '975999'},
+
+"richard" : {'email': 'richard@icloud.com', 'password': '249470', 'DocID': '547443'},
+
+"william" : {'email': 'william@icloud.com', 'password': '653334', 'DocID': '636128'},
+
+"robert" : {'email': 'robert@gmail.com', 'password': '307968', 'DocID': '283225'},
+
+"michael" : {'email': 'michael@gmail.com', 'password': '678136', 'DocID': '664056'},
+
+"david" : {'email': 'david@icloud.com', 'password': '230031', 'DocID': '265466'},
+
+"stephen" : {'email': 'stephen@icloud.com', 'password': '660196', 'DocID': '917119'},
+
+"jeffrey" : {'email': 'jeffrey@icloud.com', 'password': '227062', 'DocID': '880658'},
+
+"charles" : {'email': 'charles@icloud.com', 'password': '256935', 'DocID': '136025'},
+
+"daniel" : {'email': 'daniel@icloud.com', 'password': '582863', 'DocID': '560825'},
+
+"steven" : {'email': 'steven@gmail.com', 'password': '720492', 'DocID': '774657'},
+
+"paul" : {'email': 'paul@icloud.com', 'password': '839491', 'DocID': '464418'},
+
+"elizabeth" : {'email': 'elizabeth@gmail.com', 'password': '459807', 'DocID': '509446'},
+
+"susan" : {'email': 'susan@gmail.com', 'password': '331193', 'DocID': '551320'},
+
+"mary" : {'email': 'mary@gmail.com', 'password': '323856', 'DocID': '972752'},
+
+"joseph" : {'email': 'joseph@icloud.com', 'password': '717302', 'DocID': '165582'},
+
+"mark" : {'email': 'mark@gmail.com', 'password': '938772', 'DocID': '195681'},
+
+"thomas" : {'email': 'thomas@icloud.com', 'password': '318568', 'DocID': '583772'},
+ 
+ 
     "user": {
         'email': 'user@email.com',
         'password': '1234',
@@ -318,4 +356,22 @@ def AverageCalories(User):
 
 
 if __name__ == '__main__':
+    import random
     app.run(host='0.0.0.0')
+    # for key in AppointmentDB:
+    #     Domains=[
+    #         '@gmail.com',
+    #         '@icloud.com',
+
+  
+          
+    #     ]
+    #     d={
+    #         f'{key.lower()}':{
+    #             "email":f'{key.lower()}{Domains[random.randint(0,len(Domains)-1)]}',
+    #             "password":str(random.randint(100000, 999999)),
+    #             "DocID":str(random.randint(100000, 999999))
+    #        }
+    #        }
+       
+    #     print('"'+key.lower()+'" : '+str(d[key.lower()])+',\n')

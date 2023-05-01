6116f89096422a8de685ce4a73377de031cde3ae
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-05-01 06:18:19 +0530

    Change Server

--- Scripts/Config.js
+++ Scripts/Config.js
@@ -1,4 +1,4 @@
-SERVER_URL='http://127.0.0.1:5000'
+// SERVER_URL='http://127.0.0.1:5000'
 //SERVER_URL='https://20b9-117-241-252-25.ngrok-free.app'
-// SERVER_URL='https://shearogyaserver.onrender.com'
+SERVER_URL='https://shearogyaserver.onrender.com'
 

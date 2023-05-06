580538153ed2fa89dfd9c1d3728bd2371f314107
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-05-06 18:30:44 +0530

    Encrypt

--- Main/GlobalChat.js
+++ Main/GlobalChat.js
@@ -2,7 +2,7 @@ let sound=new Audio('../../media/Audio/MessagePopup.mp3')
 
 
 const socket = new WebSocket(
-    "ws://" + "127.0.0.1:5000" + "/echo/" + Sender + "/" + "Isacc"
+    "wss://" + "127.0.0.1:5000" + "/echo/" + Sender + "/" + "Isacc"
 );
 
 socket.addEventListener("message", (ev) => {

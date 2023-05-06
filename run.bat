dd17872890b3d296cda39fd92738cdda4fa07763
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-05-06 23:00:02 +0530

    Update Run

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@
 @REM echo 'Activate VENV'
 @REM start "Reverse Proxy" ngrok.exe http 5000
 start "FrontEnd" venv\Scripts\python.exe -m http.server   
-@REM cd Server
+
 start "Server" py Server\app.py
 start chrome http://127.0.0.1:5000
 start chrome http://127.0.0.1:8000

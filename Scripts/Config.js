9a386fc601db25facc67f3bd9a1d9aa427d9f710
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-05-06 21:50:38 +0530

    Config

--- Scripts/Config.js
+++ Scripts/Config.js
@@ -1,4 +1,4 @@
-SERVER_URL='http://127.0.0.1:5000'
+// SERVER_URL='http://127.0.0.1:5000'
 // SERVER_URL='https://e574-2a09-bac5-3d4a-1282-00-1d8-1b8.ngrok-free.app/'
-// SERVER_URL='https://shearogyaserver.onrender.com'
+SERVER_URL='https://shearogyaserver.onrender.com'
 

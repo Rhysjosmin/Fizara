5183a07bb5c7816a0358b9728d9d8668fd60b0bf
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-05-06 18:33:00 +0530

    wss Fix

--- Main/GlobalChat.js
+++ Main/GlobalChat.js
@@ -2,7 +2,7 @@ let sound=new Audio('../../media/Audio/MessagePopup.mp3')
 
 
 const socket = new WebSocket(
-    "wss://" + "127.0.0.1:5000" + "/echo/" + Sender + "/" + "Isacc"
+    "wss" + SERVER_URL.replace('https','') + "/echo/" + Sender + "/" + "Isacc"
 );
 
 socket.addEventListener("message", (ev) => {

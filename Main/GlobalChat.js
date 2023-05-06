e78ca1c795e9829ac3eb19a570e77588c3205adc
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-05-06 21:53:16 +0530

    WSS

--- Main/GlobalChat.js
+++ Main/GlobalChat.js
@@ -1,6 +1,6 @@
 let sound=new Audio('../../media/Audio/MessagePopup.mp3')
 
-let wbskt="ws" + SERVER_URL.replace('https','').replace('http','') + "/echo/" + Sender + "/" + 'ActiveChat'
+let wbskt="wss" + SERVER_URL.replace('https','').replace('http','') + "/echo/" + Sender + "/" + 'ActiveChat'
 console.log(wbskt)
 const socket = new WebSocket(
     wbskt

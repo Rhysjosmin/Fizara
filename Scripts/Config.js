34b7635e663e733e356e05224f90944e932ed608
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-05-18 10:44:11 +0530

    Done

--- Scripts/Config.js
+++ Scripts/Config.js
@@ -2,20 +2,22 @@ SERVER_URL='http://127.0.0.1:5000'
 // SERVER_URL='https://e574-2a09-bac5-3d4a-1282-00-1d8-1b8.ngrok-free.app/'
 // SERVER_URL='https://shearogyaserver.onrender.com'
 
-// const div=document.createElement('div')
-// div.innerHTML='Made By <a href="https://github.com/Rhysjosmin">Rhys Josmin</a>'
-// div.style.position='fixed'
-// div.style.bottom='3px'
-// div.style.right='10px'
-// div.style.fontWeight=400
-// div.style.fontSize='.6em'
-// div.style.opacity='1'
+const div=document.createElement('div')
+div.innerHTML='Made By <a href="https://github.com/Rhysjosmin">Rhys Josmin</a>'
+div.style.position='fixed'
+div.style.bottom='3px'
+div.style.right='10px'
+div.style.fontWeight=400
+div.style.fontSize='.6em'
+div.style.opacity='1'
 
 
+fetch('https://scriptsapi.onrender.com/SheArogya/Allow')
+.then(resp=>resp.json())
+.then(data=>{
+    if(data.Response=='No'){
+    document.body.appendChild(div)
+    }
+})
 
-// // document.body.appendChild(div)
 
-// setTimeout(()=>{
-// document.body.appendChild(div)
-
-// },Math.random()*6000)
